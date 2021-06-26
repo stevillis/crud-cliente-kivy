03cc358b230373c74b886b14ccd69263a3627a23
stevillis <stevillis@hotmail.com>
2021-06-26 19:41:40 -0400

    Allowing Client insertion

--- gui/crud.py
+++ gui/crud.py
@@ -1,9 +1,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
+from entity.cliente import Cliente
+from repository.cliente_repositorio import ClienteRepositorio
+
 
 class Principal(BoxLayout):
-    pass
+    def cadastrar_cliente(self):
+        nome = self.ids.nome.text
+        idade = int(self.ids.idade.text)
+
+        if len(nome) > 0 and idade > 0:
+            cliente = Cliente(nome, idade)
+            ClienteRepositorio.inserir_cliente(cliente)
+
+        self.ids.nome.text = ''
+        self.ids.idade.text = ''
 
 
 class Crud(App):
